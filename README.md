f97451b6a552d4af77689ea95666ee718be25e3e
brian bower <brianbowerweb@gmail.com>
2014-12-05 19:45:44 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-FreeBSD-Setup
-=============
+FreeBSD Basic Setup
+===================
+
+This is a very simple script I use to help the setup of a web server on FreeBSD. You'll notice it doesn't automate much, it just takes you through a few steps. It is not meant to guide through a fully configured server, it's a basic startup.
---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+echo "Change root password..."
+passwd
+echo "Fetch and extract ports..."
+portsnap fetch
+portsnap extract
+echo "Install nano..."
+cd /usr/ports/editors/nano
+make install clean
+echo "Change hostname..."
+nano /etc/rc.conf
+echo "Install sudo..."
+cd /usr/ports/security/sudo
+make install clean
+echo "Setup user..."
+adduser
+echo "Add user to sudoers..."
+visudo
+echo "Disable root login..."
+nano /etc/ssh/sshd_config
+echo "Install nginx..."
+cd /usr/ports/www/nginx
+make install clean
+echo "Add nginx_enable=YES..."
+nano /etc/rc.conf
